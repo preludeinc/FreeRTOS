dec871a903944b75e60cd40d7b8291633bb47d33
preludeinc <71677968+preludeinc@users.noreply.github.com>
2021-01-27 14:56:28 -0800

    Revised Spacing in Purpose Section

--- main.c
+++ main.c
@@ -7,8 +7,8 @@
   * @Project        : freertos
   *
   * Purpose         : RTOS is used to toggle the on-board blue LED at a 250 ms rate. The blinkRed task toggles
-  * 				          the on-board red LED at a 125 ms rate, blinkGreen toggles the greenLED at a 75 ms rate, blinkOrange
-  * 				          toggles the yellowLED at a 37 ms rate.
+  * 		      the on-board red LED at a 125 ms rate, blinkGreen toggles the greenLED at a 75 ms rate, blinkOrange
+  * 		      toggles the yellowLED at a 37 ms rate.
   ******************************************************************************
   */
 /* USER CODE END Header */
